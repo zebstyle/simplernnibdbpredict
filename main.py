e3993210f39ca1680431786b95f15f79757dd6b9
zebstyle <40661157+zebstyle@users.noreply.github.com>
2025-10-03 20:39:07 +0530

    Update main.py
    updated

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 
 # Load the pre-trained model with ReLU activation
-model = load_model('C://Users//06774E744//Downloads//simple_rnn_imdb//imdb_rnn_model.h5')
+model = load_model('imdb_rnn_model.h5')
 
 # Step 2: Helper Functions
 # Function to decode reviews
@@ -47,4 +47,5 @@ if st.button('Classify'):
     st.write(f'Prediction Score: {prediction[0][0]}')
 else:
     st.write('Please enter a movie review.')
+
 
